5d8c2c56a43178e717176c2d7894c9c0626cee92
VyrekXD <vyrekxd@gmail.com>
2021-07-05 13:12:58 -0500

    Fix

--- src/Lib/config.ts
+++ src/Lib/config.ts
@@ -2,7 +2,11 @@ import { config as InitializeENV } from 'dotenv'
 
 if (!process.env.TOKEN) InitializeENV()
 
+const Token = process.argv.includes('DEV=true')
+	? process.env.TOKEN_DEV
+	: process.env.TOKEN
+
 export const Config = {
-	token: process.env.TOKEN,
+	token: Token,
 	devs: process.env.DEVS?.split(',')
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -4,6 +4,7 @@ declare global {
 	namespace NodeJS {
 		interface ProcessENV {
 			TOKEN: string
+			TOKEN_DEV: string
 			DEVS: string
 			INVITE: string
 		}
